fb1ed64537c79acd2dcf59bb9a58216f9bd73e87
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-16 14:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 const makeChange = (c) => {
-  
-	let total = parseInt(c)
-	let obj = {
-		q: 25,
-		d: 10,
-		n: 5,
-		p: 1
-	}
-	let result = {
-		q:0,
-		d:0,
-		n:0,
-		p:0
-	}
-	while(total >0){
-		for(let item in obj){
-			if(total >= obj[item]){
-				result = result[item] + 1
-				total -= obj[item]
-				break;
-			}
-			}
-			
-		}
-	}
-return result
+    let total = parseInt(c);
+    let obj = {
+        q: 25,
+        d: 10,
+        n: 5,
+        p: 1
+    };
+    let result = {
+        q: 0,
+        d: 0,
+        n: 0,
+        p: 0
+    };
+
+    while (total > 0) {
+        for (let item in obj) {
+            if (total >= obj[item]) {
+                result[item] += 1;
+                total -= obj[item];
+                break;
+            }
+        }
+    }
+    return result;
 };
 
-// Do not the change the code below
 const c = prompt("Enter c: ");
-alert(console.log(JSON.stringify(makeChange(c))));
+alert(JSON.stringify(makeChange(c)));
+
