16adece3bb63d6ec547bf3e13946abe3396ecc28
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-16 14:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const makeChange = (c) => {
-  // your name here
+  
+	let total = parseInt(c)
+	let obj = {
+		q: 25,
+		d: 10,
+		n: 5,
+		p: 1
+	}
+	let result = {
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	}
+	while(total >0){
+		for(let item in obj){
+			if(total >= obj[item]){
+				result = result[item] + 1
+				total -= obj[item]
+				break;
+			}
+			}
+			
+		}
+	}
+return result
 };
 
 // Do not the change the code below
